f1bc4f7bba355b8bb26c15b88b5511e7cd7c29d9
Rashidprojects <rashidgitproject@gmail.com>
2024-04-14 23:43:10 +0530

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,11 @@
 import React from 'react'
-import { Swiper,SwiperSlide } from 'swiper/react'
-import project1 from '../assets/project-1.png'
-import 'swiper/css'
+
 
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-      <div>
-      <Swiper 
-            spaceBetween={50}
-            slidesPerView={2}
-            onSlideChange={() => console.log('slide changed') }
-            onSwiper={(swiper) => console.log(swiper)}
-      
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-      </Swiper>
-      </div>
+    <div className='bg-gray-800 text-sm p-4 text-center text-li-white font-semibold'>
+      Copyright © 2024 Rashid.All Rights reserved
     </div>
     
   )
