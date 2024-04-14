5d66c1969b6deab998c9d4a966a335e544a0d5de
Rashidprojects <rashidgitproject@gmail.com>
2024-04-14 17:37:16 +0530

    skill page design completed

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
-    </div>
+    <section id='projects' className='py-10 text-li-white'>
+      <div className='text-center'>
+        <h3 className='text-4xl font-semibold'>
+          My <span className='text-cyan'>Projects</span>
+        </h3>
+        <p className='text-gray-400 mt-3 text-lg'>My awesome works</p>
+      </div>
+    </section>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
       'li-white' : '#f1f5f9',
       'gray-600' : '#4b5563',
       'gray-400' : '#9ca3af',
-      'gray-300' : '#d1d5db'
+      'gray-300' : '#d1d5db',
+      'gray-800' : '#1f2937',
+      'gray-100' : '#f3f4f6',
+      'gray-900' : '#111827',
     }
   },
   plugins: [require("daisyui")],
